bcda6de53a3cb8df512211cfcb9fe16850766ecf
Juseong Park <juseongkr@gmail.com>
2021-03-03 21:59:29 +0900

    Refactor: refactoring and linting (#7)

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,171 +1,215 @@
-import express, { Request, Response, NextFunction, Router } from 'express';
-import { Document } from 'mongoose';
-import User from '../models/user.model';
-import middleware from '../utils/middlewares';
-import logger from '../utils/logger';
-import { userValidation, validate } from '../utils/validator';
-import { hashPassword, updateUserInfo, validateUserPassword } from '../services/auth.service';
+import express, { Request, Response, NextFunction, Router } from "express";
+import User from "../models/user.model";
+import middleware from "../utils/middlewares";
+import logger from "../utils/logger";
+import { userValidation, validate } from "../utils/validator";
+import {
+  hashPassword,
+  updateUserInfo,
+  validateUserPassword,
+} from "../services/auth.service";
+import { StatusCodes } from "http-status-codes";
 
 const authRouter: Router = express.Router();
 
-authRouter.get('/check',
-    middleware.isLoggedIn,
-    (req: Request, res: Response): void => {
-    const session: Express.Session | undefined = req.session;
-    if (session?.user) {
-        res.json({ username: session.user.username, name: session.user.name, id: session.user.id });
-    } else {
-        res.status(401).end();
-    }
-});
+authRouter.get(
+  "/check",
+  middleware.isLoggedIn,
+  (req: Request, res: Response): void => {
+    const session = req.session;
 
-authRouter.post('/register',
-    middleware.isNotLoggedIn,
-    validate(userValidation),
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (session?.user) {
+      res.json({
+        username: session.user.username,
+        name: session.user.name,
+        id: session.user.id,
+      });
+    } else {
+      res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  }
+);
+
+authRouter.post(
+  "/register",
+  middleware.isNotLoggedIn,
+  validate(userValidation),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { username, password, name } = req.body;
 
     try {
-        const user: Document | null = await User.findOne({ username });
-        if (user) {
-            res.status(409).json({
-                error: 'username conflict',
-            });
-            return;
-        }
+      const user = await User.findOne({ username });
+      if (user) {
+        res.status(StatusCodes.CONFLICT).json({
+          error: "username conflict",
+        });
+        return;
+      }
 
-        await new User({
-            password: await hashPassword(password),
-            username,
-            name,
-        }).save();
+      await new User({
+        password: await hashPassword(password),
+        username,
+        name,
+      }).save();
 
-        res.json({ username, name });
+      res.json({ username, name });
     } catch (err) {
-        logger.error(err);
-        next(err);
+      logger.error(err);
+      next(err);
     }
-});
+  }
+);
 
-authRouter.post('/login',
-    middleware.isNotLoggedIn,
-    validate(userValidation),
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+authRouter.post(
+  "/login",
+  middleware.isNotLoggedIn,
+  validate(userValidation),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { username, password } = req.body;
 
     try {
-        const user: Document | null = await validateUserPassword({ username, password });
-        if (!user) {
-            res.status(401).json({
-                error: 'Unauthorized access',
-            });
-            return;
-        }
+      const user = await validateUserPassword({
+        username,
+        password,
+      });
 
-        const updated: Document | null = await updateUserInfo({ username }, {
-            ...user.toJSON(),
-            ip_address: req.ip,
-            last_login: new Date().toISOString(),
+      if (!user) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          error: "Unauthorized access",
         });
-        if (updated) {
-            req.session!.user = {
-                id: user.get('id'),
-                name: user.get('name'),
-                username,
-            };
-        }
+        return;
+      }
 
-        res.json({ username, id: user.get('id'), name: user.get('name') });
+      const updated = await updateUserInfo(
+        { username },
+        {
+          ...user.toJSON(),
+          ip_address: req.ip,
+          last_login: new Date().toISOString(),
+        }
+      );
+      if (updated) {
+        req.session!.user = {
+          id: user.get("id"),
+          name: user.get("name"),
+          username,
+        };
+      }
+
+      res.json({ username, id: user.get("id"), name: user.get("name") });
     } catch (err) {
-        logger.error(err);
-        next(err);
+      logger.error(err);
+      next(err);
     }
-});
+  }
+);
 
-authRouter.post('/logout',
-    middleware.isLoggedIn,
-    (req: Request, res: Response, next: NextFunction): void => {
-    req.session!.destroy(err => {
-        if (err) {
-            logger.error(err);
-            next();
-        } else {
-            res.clearCookie('session-cookie').redirect('/');
-        }
+authRouter.post(
+  "/logout",
+  middleware.isLoggedIn,
+  (req: Request, res: Response, next: NextFunction): void => {
+    req.session!.destroy((err) => {
+      if (err) {
+        logger.error(err);
+        next();
+      } else {
+        res.clearCookie("session-cookie").redirect("/");
+      }
     });
-});
+  }
+);
 
-authRouter.put('/reset',
-    middleware.isLoggedIn,
-    validate(userValidation),
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+authRouter.put(
+  "/reset",
+  middleware.isLoggedIn,
+  validate(userValidation),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { username, password, oldPassword, name } = req.body;
 
     try {
-        const user: Document | null = await validateUserPassword({ username, password: oldPassword });
-        if (!user) {
-            res.status(401).json({
-                error: 'Unauthorized access',
-            });
-            return;
-        }
-
-        const updated: Document | null = await updateUserInfo({ username }, {
-            ...user.toJSON(),
-            password: await hashPassword(password),
-            name,
+      const user = await validateUserPassword({
+        username,
+        password: oldPassword,
+      });
+      if (!user) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          error: "Unauthorized access",
         });
-        if (updated) {
-            req.session!.destroy(err => {
-                if (err) {
-                    logger.error(err);
-                    next();
-                } else {
-                    res.clearCookie('session-cookie').end();
-                }
-            });
-        }
-    } catch (err) {
-        logger.error(err);
-        next(err);
-    }
-});
+        return;
+      }
 
-authRouter.delete('/unregister',
-    middleware.isLoggedIn,
-    validate(userValidation),
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+      const updated = await updateUserInfo(
+        { username },
+        {
+          ...user.toJSON(),
+          password: await hashPassword(password),
+          name,
+        }
+      );
+
+      if (updated) {
+        req.session!.destroy((err) => {
+          if (err) {
+            logger.error(err);
+            next();
+          } else {
+            res.clearCookie("session-cookie").end();
+          }
+        });
+      }
+    } catch (err) {
+      logger.error(err);
+      next(err);
+    }
+  }
+);
+
+authRouter.delete(
+  "/unregister",
+  middleware.isLoggedIn,
+  validate(userValidation),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { username, password } = req.body;
+
     try {
-
-        const user: Document | null = await validateUserPassword({ username, password });
-        if (!user) {
-            res.status(401).json({
-                error: 'Unauthorized access',
-            });
-            return;
-        }
-
-        const deleted: Document | null = await updateUserInfo({ username }, {
-            ...user.toJSON(),
-            deactivated: true,
+      const user = await validateUserPassword({
+        username,
+        password,
+      });
+      if (!user) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+          error: "Unauthorized access",
         });
-        if (deleted) {
-            req.session!.destroy(err => {
-                if (err) {
-                    logger.error(err);
-                    next();
-                } else {
-                    res.clearCookie('session-cookie').status(204).end();
-                }
-            });
-        } else {
-            res.status(400).end();
+        return;
+      }
+
+      const deleted = await updateUserInfo(
+        { username },
+        {
+          ...user.toJSON(),
+          deactivated: true,
         }
+      );
+      if (deleted) {
+        req.session!.destroy((err) => {
+          if (err) {
+            logger.error(err);
+            next();
+          } else {
+            res
+              .clearCookie("session-cookie")
+              .status(StatusCodes.NO_CONTENT)
+              .end();
+          }
+        });
+      } else {
+        res.status(StatusCodes.BAD_REQUEST).end();
+      }
     } catch (err) {
-        logger.error(err);
-        next(err);
+      logger.error(err);
+      next(err);
     }
-});
+  }
+);
 
 export default authRouter;
--- server/src/services/authors.service.ts
+++ server/src/services/authors.service.ts
@@ -1,26 +1,37 @@
-import { Document } from 'mongoose';
-import { Author as AuthorType } from '../types';
-import Author from '../models/author.model';
-import { AuthorDto } from '../types';
+import { Document } from "mongoose";
+import { Author as AuthorType } from "../types";
+import Author from "../models/author.model";
+import { AuthorDto } from "../types";
 
-export const updateAuthor = async (newAuthor: AuthorType): Promise<Document | null> => {
-    const { ssn, uploader } = newAuthor;
-    return await Author.findOneAndUpdate({ ssn, uploader }, newAuthor, { new: true });
-}
+export const updateAuthor = async (
+  newAuthor: AuthorType
+): Promise<Document | null> => {
+  const { ssn, uploader } = newAuthor;
 
-export const deleteAuthor = async (authorDto: AuthorDto): Promise<Document | null> => {
-    const { ssn, uploader } = authorDto;
-    return await Author.findOneAndDelete({ ssn, uploader });
-}
+  return await Author.findOneAndUpdate({ ssn, uploader }, newAuthor, {
+    new: true,
+  });
+};
 
-export const addAuthor = async (newAuthor: AuthorType): Promise<Document | null> => {
-    const { ssn } = newAuthor;
-    const duplicated: Document | null = await Author.findOne({ ssn });
-    if (duplicated) {
-        return null;
-    }
-    const savedAuthor: Document | null = await new Author({
-        ...newAuthor,
-    }).save();
-    return savedAuthor;
-}
+export const deleteAuthor = async (
+  authorDto: AuthorDto
+): Promise<Document | null> => {
+  const { ssn, uploader } = authorDto;
+
+  return await Author.findOneAndDelete({ ssn, uploader });
+};
+
+export const createAuthor = async (
+  newAuthor: AuthorType
+): Promise<Document | null> => {
+  const { ssn } = newAuthor;
+  const duplicated = await Author.findOne({ ssn });
+
+  if (duplicated) {
+    return null;
+  }
+
+  return await new Author({
+    ...newAuthor,
+  }).save();
+};
